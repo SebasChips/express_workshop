ea5062e6480fbf483a5989af05808467fa2f35e5
SebasChips <sebastianalarconbejar@gmail.com>
2023-03-24 18:19:25 -0600

    initial commit

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ DELETE
 */
 app.get("/", (req,res,next)=>{
     res.status(200);
-    res.send("Bienvenido");
+    res.send("B3ienvenido a mi server jijijjij");
 });
+
+app.get("/:nombre", (req, res, next) =>{
+    res.status(200)
+    res.send("Estás en la página 'nombre' ");
+})
+
 app.listen(3000, () =>{
     console.log("Server is running...")
 });
