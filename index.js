4f65191c2dadcb2028b78be25a2da91e908156d7
SebasChips <sebastianalarconbejar@gmail.com>
2023-03-24 18:27:36 -0600

    Actualización proyecto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const {pokemon}= require('./pokedex.json');
 /*
 VERBOS HTTP
 GET
@@ -9,16 +9,42 @@ PATCH
 PUT
 DELETE
 */
-app.get("/", (req,res,next)=>{
+app.get('/', (req,res,next)=>{
     res.status(200);
-    res.send("B3ienvenido a mi server jijijjij");
+    res.send("Hola, bienvenido a la pokedex");
 });
 
-app.get("/:nombre", (req, res, next) =>{
-    res.status(200)
-    res.send("Estás en la página 'nombre' ");
-})
+app.get('/pokemon/all', (req,res,next)=>{
+    res.status(200);
+    res.send(pokemon);
+});
 
-app.listen(3000, () =>{
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next)=>{
+    const id= req.params.id -1;
+    if (id>=0 && id <=150) {
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }
+    else {
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+    
+});
+
+app.get('/pokemon/:name', (req,res,next)=>{
+    const name = req.params.name;
+    for (i  = 0; i < pokemon.length; i++) {
+        if (pokemon[i].name == name) {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado");
+});
+
+
+app.listen(process.env.PORT || 3000,() =>{
     console.log("Server is running...")
 });
