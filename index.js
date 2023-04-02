b598eeca48f12deb56e5643ca73eb0fb17010ff6
SebasChips <sebastianalarconbejar@gmail.com>
2023-04-02 09:38:43 -0600

    up-to-02/04/23

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const bodyParser= require('body-parser');
 const express = require('express');
 const app = express();
 const {pokemon}= require('./pokedex.json');
+
 /*
 VERBOS HTTP
 GET
@@ -9,40 +11,52 @@ PATCH
 PUT
 DELETE
 */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req,res,next)=>{
-    res.status(200);
-    res.send("Hola, bienvenido a la pokedex");
+    return res.status(200).send("Hola, bienvenido a la pokedex");
 });
 
-app.get('/pokemon/all', (req,res,next)=>{
+app.post("/pokemon", (req, res, next)=>{
+    return res.status(200).send(req.body.name);
+})
+
+app.get('/pokemon', (req,res,next)=>{
     res.status(200);
-    res.send(pokemon);
+    return res.send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})', (req,res,next)=>{
     const id= req.params.id -1;
     if (id>=0 && id <=150) {
         res.status(200);
-        res.send(pokemon[req.params.id - 1]);
+        return res.send(pokemon[req.params.id - 1]);
     }
     else {
         res.status(404);
-        res.send("Pokemon no encontrado");
+        return res.send("Pokemon no encontrado");
     }
     
 });
 
-app.get('/pokemon/:name', (req,res,next)=>{
+app.get('/pokemon/:name([A-Za-z]+)', (req,res,next)=>{
     const name = req.params.name;
-    for (i  = 0; i < pokemon.length; i++) {
-        if (pokemon[i].name == name) {
-            res.status(200);
-            res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Pokemon no encontrado");
-});
+    
+    //If mamón(operadorTernario): condición ? valor si verdadero: valor si falso
+
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
+
+    (pk.length > 0 ) ? res.status(200).send(pk):res.status(200).send(pk);
+    
+    res.status(404).send("Pokemon no encontrado");
+    
+    
+    });
+  
 
 
 app.listen(process.env.PORT || 3000,() =>{
