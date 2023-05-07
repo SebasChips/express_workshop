cc8b08816a0732ea563a153873c3af1c89637d2f
SebasChips <sebastianalarconbejar@gmail.com>
2023-05-07 09:17:18 -0600

    JWT instalación

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -20,6 +21,24 @@ user.post("/", async (req,res,next) => {
 
     });
 
+user.post("/login", async(req,res,next)=>{
+    const{user_mail,user_password}= req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND WHERE user_password = ${user_password}';'`;
+    const rows = await db.query(query);
+
+    if (user_mail && user_password) {
+        if (rows.length == 1) {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({code: 200, message: token});
+        }
+        else {
+            res.status(401).json({code:401, message: "Usuario y/o contraseña incorrectos"});
+        }
+    }
+});
 
 user.get("/", async (req, res, next) =>{
     const query= "SELECT * FROM userr";
@@ -28,4 +47,4 @@ user.get("/", async (req, res, next) =>{
     return res.status(200).json({code:200, message: rows});
 })
 
-    module.exports = user;
+module.exports = user;
